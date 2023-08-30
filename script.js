b5536351e0076b469238df18a1b663acdca67950
Carter Toni <carter.blaine.toni@gmail.com>
2023-08-30 11:52:46 -0700

    Complete working first version of application

--- script.js
+++ script.js
@@ -1,43 +1,63 @@
-for (i = 0; i < 16; i++) {
-  const container = document.querySelector('.container')
-  const newDiv = document.createElement('div')
-  newDiv.classList.add('row')
-  newDiv.style.display = 'flex'
-  container.appendChild(newDiv)
+const addEventListeners = () => {
+  const squares = document.querySelectorAll('.square')
+  squares.forEach(square =>
+    square.addEventListener(
+      'mouseover',
+      () => (square.style.backgroundColor = 'black')
+    )
+  )
 }
 
-const rows = document.querySelectorAll('.row')
+const drawNewGrid = sides => {
+  const squareSize = 960 / sides
 
-rows.forEach(row => {
-  for (i = 0; i < 16; i++) {
+  for (i = 0; i < sides; i++) {
+    const container = document.querySelector('.container')
     const newDiv = document.createElement('div')
-    newDiv.classList.add('square')
-    newDiv.style.backgroundColor = 'white'
-    newDiv.style.height = '96px'
-    newDiv.style.flex = '0 0 96px'
-    row.appendChild(newDiv)
+    newDiv.classList.add('row')
+    newDiv.style.display = 'flex'
+    container.appendChild(newDiv)
   }
-})
 
-const squares = document.querySelectorAll('.square')
-console.log(squares)
-squares.forEach(square =>
-  square.addEventListener(
-    'mouseover',
-    () => (square.style.backgroundColor = 'black')
-  )
-)
+  const rows = document.querySelectorAll('.row')
+
+  rows.forEach(row => {
+    for (i = 0; i < sides; i++) {
+      const newDiv = document.createElement('div')
+      newDiv.classList.add('square')
+      newDiv.style.backgroundColor = 'white'
+      newDiv.style.height = `${squareSize}px`
+      newDiv.style.flex = `0 0 ${squareSize}px`
+      row.appendChild(newDiv)
+    }
+    addEventListeners()
+  })
+}
+
+drawNewGrid(16)
 
 const resetGrid = () => {
+  const container = document.querySelector('.container')
   const squares = document.querySelectorAll('.square')
-  squares.forEach(square => (square.style.backgroundColor = 'white'))
+  squares.forEach(square => {
+    container.remove(square)
+  })
+  const body = document.querySelector('body')
+  const newContainer = document.createElement('div')
+  newContainer.classList.add('container')
+  body.appendChild(newContainer)
 }
 
 const getNewGridSize = () => {
-  console.log('test')
-  const sides = prompt('Enter number of squares per side of new grid')
-  console.log(sides)
+  let sides
+  do {
+    sides = parseInt(
+      prompt('Enter number of squares per side of new grid (max 100)')
+    )
+  } while (!Number.isInteger(sides) || sides <= 0 || sides > 100)
+
   resetGrid()
+  drawNewGrid(sides)
 }
 
 const resizeGrid = document.querySelector('.resize-grid')
